f61ac074f94ca1d1797a35d3e723abdbed493b43
yumetodo <yume-wikijp@live.jp>
2016-12-26 13:32:57 +0900

    implement random_generator_iterator_with_count class

--- random_generator_iterator.hpp
+++ random_generator_iterator.hpp
@@ -68,6 +68,56 @@ public:
 	constexpr bool operator!=(const random_generator_iterator& r) const noexcept { return !(*this == r); }
 };
 template<typename T>
+class random_generator_iterator_with_count 
+#if __cplusplus < 201500 //C++17ではstd::iteratorは消える
+	: public std::iterator<std::input_iterator_tag, T>
+#endif
+{
+#if __cplusplus < 201500
+private:
+	using base_type = std::iterator<std::input_iterator_tag, T>;
+public:
+	using iterator_category = typename base_type::iterator_category;
+	using value_type = typename base_type::value_type;
+	using difference_type = typename base_type::difference_type;
+	using pointer = typename base_type::pointer;
+	using reference = typename base_type::reference;
+#else
+public:
+	using iterator_category = typename std::iterator_traits<Iterator>::iterator_category;
+	using value_type = T;
+	using difference_type = std::ptrdiff_t;
+	using pointer = T*;
+	using reference = T&;
+#endif
+private:
+	std::size_t count_;
+	random_generator_iterator<value_type> it_;
+public:
+	random_generator_iterator_with_count() = delete;
+	constexpr random_generator_iterator_with_count(std::size_t n) noexcept : count_(n), it_() {}
+	random_generator_iterator_with_count(const random_generator_iterator_with_count&) = delete;
+	random_generator_iterator_with_count(random_generator_iterator_with_count&&) = default;
+	random_generator_iterator_with_count& operator=(const random_generator_iterator_with_count&) = delete;
+	random_generator_iterator_with_count& operator=(random_generator_iterator_with_count&&) = default;
+	random_generator_iterator_with_count(value_type min, value_type max, std::mt19937& mt)
+		: count_(), it_(min, max, mt) {}
+	value_type operator*() { return *(this->it_); }
+	random_generator_iterator_with_count& operator++() noexcept
+	{
+		++this->count_;
+		return *this;
+	}
+	random_generator_iterator_with_count operator++(int) noexcept
+	{
+		const auto re = *this;
+		++this->count_;
+		return re;
+	}
+	constexpr bool operator==(const random_generator_iterator_with_count& r) const noexcept { return this->count_ == r.count_; }
+	constexpr bool operator!=(const random_generator_iterator_with_count& r) const noexcept { return !(*this == r); }
+};
+template<typename T>
 class random_generator_range {
 public:
 	using value_type = T;
