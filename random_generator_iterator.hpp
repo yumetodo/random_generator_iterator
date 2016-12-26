164ea69599d662755a1aaaaafd75c52d89222243
yumetodo <yume-wikijp@live.jp>
2016-12-26 13:16:18 +0900

    use pimpl to specfy default ctor constexpr
    std::unique_ptr's default ctor is specified constexpr. However,
    C:/Users/yumetodo/Documents/git/random_generator_iterator/main.cpp:9:43: error: constexpr variable cannot have non-literal type 'const random_generator_iterator<int>'
            constexpr random_generator_iterator<int> i;
                                                     ^
    C:/Users/yumetodo/Documents/git/random_generator_iterator/random_generator_iterator.hpp:52:24: note: 'random_generator_iterator<int>' is not literal because it has data member 'pimpl_' of non-literal type
          'std::unique_ptr<impl>'
            std::unique_ptr<impl> pimpl_;
                                  ^
    1 error generated.

--- random_generator_iterator.hpp
+++ random_generator_iterator.hpp
@@ -1,6 +1,7 @@
 ﻿#ifndef INCLUDE_RANDOM_GENERATOR_ITERATOR_HPP_
 #define INCLUDE_RANDOM_GENERATOR_ITERATOR_HPP_
 #include "random.hpp"
+#include <memory>
 template<typename T>
 class random_generator_iterator
 #if __cplusplus < 201500 //C++17ではstd::iteratorは消える
@@ -23,24 +24,35 @@ public:
 #endif
 	using distribution = uniform_normal_distribution<value_type>;
 private:
-	distribution dist_;
-	std::reference_wrapper<std::mt19937> engine_;
-	bool next_is_end_;
-#if __cplusplus < 201500
-    using base_type = std::iterator<std::input_iterator_tag, T>;
-#endif
+	struct impl {
+		distribution dist;
+		std::reference_wrapper<std::mt19937> engine;
+		bool next_is_end;
+		impl() = default;
+		impl(const impl&) = delete;
+		impl(impl&&) = default;
+		impl& operator=(const impl&) = delete;
+		impl& operator=(impl&&) = default;
+		impl(value_type min, value_type max, std::mt19937& mt) : dist(min, max), engine(mt), next_is_end(false) {}
+		value_type generate() { return this->dist(this->engine.get()); }
+	};
+	std::unique_ptr<impl> pimpl_;
+	bool is_end_iterator_;
 public:
-	random_generator_iterator() = delete;
+	constexpr random_generator_iterator() noexcept : pimpl_(), is_end_iterator_(true) {};
 	random_generator_iterator(const random_generator_iterator&) = delete;
 	random_generator_iterator(random_generator_iterator&&) = default;
 	random_generator_iterator& operator=(const random_generator_iterator&) = delete;
 	random_generator_iterator& operator=(random_generator_iterator&&) = default;
 
 	random_generator_iterator(value_type min, value_type max, std::mt19937& mt)
-		: dist_(min, max), engine_(mt), next_is_end_(false)
+		: pimpl_(std::make_unique<impl>(min, max, mt)), is_end_iterator_(false)
 	{}
-	void stop() noexcept { next_is_end_ = true; }
-	reference operator*() { return dist_(engine_); }
-	reference operator++() noexcept { return *this; }
- };
+	void stop() noexcept { this->pimpl_->next_is_end = true; }
+	value_type operator*() { return this->pimpl_->generate(); }
+	random_generator_iterator& operator++() noexcept
+	{ 
+		return *this;
+	}
+};
 #endif //INCLUDE_RANDOM_GENERATOR_ITERATOR_HPP_
