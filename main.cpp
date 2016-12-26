68a9604642c2465cfb564f8bb2dfbfec78fd8bf7
yumetodo <yume-wikijp@live.jp>
2016-12-26 13:27:54 +0900

    add sample code for random_generator_iterator and random_generator_range

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
+﻿#include "random_generator_iterator.hpp"
 #include <iostream>
-
+//random generator
+static auto engine = create_engine();
 int main(int argc, char **argv)
 {
-    std::cout << "Hello World" << std::endl;
-    return 0;
+	std::cout << "generate: random_generator_iterator" << std::endl;
+	for (auto ri = random_generator_iterator<int>(0, 10, engine); ri != random_generator_iterator<int>{}; ++ri) {
+		const auto n = *ri;
+		std::cout << n << std::endl;
+		if (0 == n) ri.stop();
+	}
+	std::cout << "generate: random_generator_range" << std::endl;
+	for (auto&& n : random_generator_range<int>(0, 10, engine)) {
+		std::cout << n << std::endl;
+		if (0 == n) break;
+	}
+	return 0;
 }
